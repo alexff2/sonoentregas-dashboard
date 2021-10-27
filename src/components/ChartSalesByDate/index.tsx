066a2d02abefd6fc13e4bba8d115e4b35ff3840b
Alexandre Silva Santos <alex_ff2@hotmail.com>
2021-10-27 08:58:27 -0300

    config chart sales by dates

--- src/components/ChartSalesByDate/index.tsx
+++ src/components/ChartSalesByDate/index.tsx
@@ -24,17 +24,6 @@ const options = {
 
 const ChartSalesByDate: React.FC = () => (
   <>
-    <div className='header'>
-      <h1 className='title'>Line Chart</h1>
-      <div className='links'>
-        <a
-          className='btn btn-gh'
-          href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/Line.js'
-        >
-          Github Source
-        </a>
-      </div>
-    </div>
     <Line data={data} options={options} />
   </>
 );
