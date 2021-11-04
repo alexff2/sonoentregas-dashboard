ad36a76a7f226a715f5fddd9a301c89d08bf4d8b
Alexandre Silva Santos <alex_ff2@hotmail.com>
2021-11-04 07:39:37 -0300

    teste de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { useRoutes, Navigate } from 'react-router-dom'
+import Header from './components/Header'
 
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 const Routes: React.FC = () => {
-  return (
-    <>
-      <Route path="/dashboard" element={<Dashboard />}/>
-    </>
-  )
+  return useRoutes([
+    {
+      path: '/app',
+      element: <Header />,
+      children: [
+        { element: <Navigate to="/app/dashboard" replace /> },
+        { path: 'dashboard', element: <Dashboard /> }
+      ]
+    },
+    {
+      path: '/',
+      element: <div />,
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: '404', element: <h1>Not Found</h1> },
+        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ])
 }
 
 export default Routes
